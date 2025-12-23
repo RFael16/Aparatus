2c1aa1f1f5a5ded46d34e9b1437ffd321b4ea024
RFael16 <frcrash7122@gmail.com>
2025-12-23 10:12:01 -0300

    feat: add booking item component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,32 @@
 import Image from "next/image";
 import Header from "./_components/header";
 import SearchInput from "./_components/search-input";
+import BookingItem from "./_components/booking-item";
 import banner from "../public/banner.png";
 
 const Home = () => {
   return (
-    <div>
+    <main>
       <Header />
       <div className="space-y-4 px-5">
         <SearchInput />
         <Image
-          src="/baner.png"
-          alt="Agende agora/"
+          src={banner}
+          alt="Agende agora!"
           sizes="100vw"
-          className="h-auto w-auto"
+          className="h-auto w-full"
+        />
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Agendamentos
+        </h2>
+        <BookingItem
+          serviceName="Corte de cabelo"
+          barbershopName="Barbearia do Jõas"
+          barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+          date={new Date()}
         />
       </div>
-    </div>
+    </main>
   );
 };
 
